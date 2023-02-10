eaab119cfa5f3531cc33c13e62ddd9cb9e672c19
hayao <hayao@fascode.net>
2023-02-10 19:05:59 +0900

    Add: example usage of go-wine

--- main.go
+++ main.go
@@ -2,11 +2,28 @@ package main
 
 import (
 	"fmt"
+	"os"
 	wine "github.com/Hayao0819/wine-cellar/go-wine"
 )
 
 
+func ePrintln(str string){
+	fmt.Fprintln(os.Stderr, str)
+}
+
 func main(){
-	local := wine.GetLocalWine
-	fmt.Println(local.arch.name)
+	homedir, err := os.UserHomeDir()
+	winedir := fmt.Sprintf("%s/.wine", homedir)
+
+	if err != nil{
+		ePrintln(err.Error())
+		os.Exit(1)
+	}
+
+	local, err:= wine.GetLocalWine("wine", winedir)
+	if err != nil{
+		ePrintln(err.Error())
+		os.Exit(1)
+	}
+	fmt.Println(local.Arch.Name)
 }
