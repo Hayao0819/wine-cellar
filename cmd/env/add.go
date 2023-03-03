28c72569a3534374f74630097c3ffe148d2010b2
hayao <hayao@fascode.net>
2023-03-03 23:31:01 +0900

    Update: set arg

--- cmd/env/add.go
+++ cmd/env/add.go
@@ -13,12 +13,12 @@ func newAddCmd()(*cobra.Command){
 		Use:   "add",
 		Short: "WINEPREFIXを登録",
 		Long: `WINEPREFIXをwine-cellarに登録し、管理対象にします。`,
+		Args: cobra.ExactArgs(2), // [name] [path]
 		RunE: func(cmd *cobra.Command, args []string) error{
 			envs, err := conf.GetEnvs()
 			if err !=nil{
 				return err
 			}
-
 
 			*envs=append(*envs, wine.Env{}) //具体的な情報付与はあとで実装する
 
